6fa99377d3c901a9f1a52f62d2cf1ca71649dde7
ExecuteAutomation <kartmcad@gmail.com>
2017-02-02 22:36:26 +0800

    Made changes to the code based on latest webdriver

--- SeleniumParallelTest/Hooks.cs
+++ SeleniumParallelTest/Hooks.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System.Reflection;
 
 namespace SeleniumParallelTest
 {
@@ -34,11 +35,26 @@ namespace SeleniumParallelTest
 
         private void ChooseDriverInstance(BrowerType browserType)
         {
+            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembl‌​y().Location);
+
             if (browserType == BrowerType.Chrome)
-                Driver = new ChromeDriver();
+                Driver = new ChromeDriver(driverDir);
             else if (browserType == BrowerType.Firefox)
-                Driver = new FirefoxDriver();
+            {
+                FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(driverDir, "geckodriver.exe");
+                service.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+                service.HideCommandPromptWindow = true;
+                service.SuppressInitialDiagnosticInformation = true;
+                Driver = new FirefoxDriver(service);
+            }
         }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+        }
+
 
     }
 }
